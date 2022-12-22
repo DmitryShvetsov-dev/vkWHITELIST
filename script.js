9b62ac91479bd3c8ae01cb29e1d6aa356df81fd0
Dmitry Shvetsov <115499772+DmitryShvetsov-dev@users.noreply.github.com>
2022-12-23 00:54:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function colorGrisha() {
+  let background = document.getElementById("grishaBox");
+  let button = document.getElementById("grishaButton");
+  background.style.backgroundColor = "rgb(53, 241, 53)";
+  button.style.display = "none";
+}
+function colorGosha() {
+  let background = document.getElementById("goshaBox");
+  let button = document.getElementById("goshaButton");
+  background.style.backgroundColor = "rgb(53, 241, 53)";
+  button.style.display = "none";
+}
+
+function colorMasha() {
+  let background = document.getElementById("mashaBox");
+  let button = document.getElementById("mashaButton");
+  background.style.backgroundColor = "rgb(53, 241, 53)";
+  button.style.display = "none";
+}
